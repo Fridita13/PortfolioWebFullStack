add44eda5b775a0852ceac8f358de6f42706c761
Frida <frida.cernigoi@gmail.com>
2022-04-17 16:12:55 -0300

    boton ingreso

--- PortfolioWEbFullStack/src/app/app.module.ts
+++ PortfolioWEbFullStack/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
-import { RedessocialesComponent } from './components/redessociales/redessociales.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaLaboralComponent } from './components/experiencia-laboral/experiencia-laboral.component';
@@ -20,7 +19,6 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    RedessocialesComponent,
     BannerComponent,
     AcercaDeComponent,
     ExperienciaLaboralComponent,
